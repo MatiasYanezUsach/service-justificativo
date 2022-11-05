7b4e38f3d010a09c9f8f7215697ae96fbcc4e1a7
Matias Yanez <matias.yanez@usach.cl>
2022-11-05 14:57:33 -0300

    inicializacion del microservicio justificativo

---
+++ src/main/java/proyecto/mingeso/microservicejustificativo/MicroserviceJustificativoApplication.java
@@ -0,0 +1,13 @@
+package proyecto.mingeso.microservicejustificativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceJustificativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceJustificativoApplication.class, args);
+	}
+
+}
