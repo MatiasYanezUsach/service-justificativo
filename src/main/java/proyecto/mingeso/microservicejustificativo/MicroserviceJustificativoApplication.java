db7613099ed40eb3bdd7784f6400b4051b3c1bb4
Matias Yanez <matias.yanez@usach.cl>
2022-11-23 18:03:14 -0300

    test

--- src/main/java/proyecto/mingeso/microservicejustificativo/MicroserviceJustificativoApplication.java
+++ src/main/java/proyecto/mingeso/microservicejustificativo/MicroserviceJustificativoApplication.java
@@ -2,10 +2,10 @@ package proyecto.mingeso.microservicejustificativo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class MicroserviceJustificativoApplication {
 
 	public static void main(String[] args) {
